97762483926121f0c3229c8a83c747912f80e96e
Daniel Proaño Chacón <danieluxury@gmail.com>
2023-02-26 08:28:01 -0500

    Add local storage to counter html

--- counter.js
+++ counter.js
@@ -1,14 +1,19 @@
-let counter = 0;
-function count() {
-    const heading = document.querySelector('h1')
-    counter++;
-    heading.innerHTML = counter;
-    if (counter % 10 === 0 ) {
-        alert(`Count is now ${counter}`);
-    }
+
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0)
 }
+
+
+function count() {
+    let counter = localStorage.getItem('counter');
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     // document.querySelector('button').addEventListener('click',count)
     document.querySelector('button').onclick = count;
-    setInterval(count, 1000);
+    // setInterval(count, 1000);
 });
